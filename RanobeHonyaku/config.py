41f0e45755accc22daa3f13c038bc421afd8b52f
cloudiirain <muffinzapplepear@gmail.com>
2016-09-23 21:06:40 -0400

    Flask-SQLAlchemy example (quickstart/tutorial) is working. Refactored project organization so the app startup logic is separate from the views. Moved admin, api, and epub into a views subdirectory. Added a config file that imports from the json (mostly because the tutorials I've been working off of do this, because the import becomes much easier that way). Added requirements.txt.

---
+++ RanobeHonyaku/config.py
@@ -0,0 +1,9 @@
+import os
+from utils import setup_file
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SECRET_KEY = setup_file["SECRET_KEY"]
+
+# This database should be for development only
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
---
+++ RanobeHonyaku/views/home.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+from utils import setup_file, apps
+
+home = Blueprint("home", __name__)
+
+# The root of the domain
+@home.route(rule="/")
+def index():
+    return render_template("home.html", setup_file=setup_file)
+
+# The route for our page of approved applications
+@home.route(rule="/applications")
+def applications():
+    return render_template("applications.html", setup_file=setup_file, applications=apps)
